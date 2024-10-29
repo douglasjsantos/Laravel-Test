de7dc3dff63bb5ce5d58a28030751c4b6f51b9b7
Douglas Santos <douglas_j.santos@hotmail.com>
2024-10-29 18:52:11 -0300

    passagens de parametros do controlador para view

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Facade\FlareClient\View;
 use Illuminate\Http\Request;
 
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2){
-        echo "A soma de $p1 + $p2 é: ". ($p1 + $p2);
+        // return View('site.teste', ['p1' => $p1, 'p2' => $p2]); ARRAY ASSOATIVO
+        return View('site.teste', compact('p1', 'p2'));
+
     }
 }
